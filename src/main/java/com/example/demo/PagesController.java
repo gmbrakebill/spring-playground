ff4f3a6df576c765d972823725b91eee907619ce
gmbrakebill <64878965+gmbrakebill@users.noreply.github.com>
2020-07-20 13:59:01 -0500

    calculate with query strring

--- src/main/java/com/example/demo/PagesController.java
+++ src/main/java/com/example/demo/PagesController.java
@@ -1,14 +1,72 @@
 package com.example.demo;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.List;
 
 @RestController
 public class PagesController {
+    @PostMapping("/math2/sum2")
+    public String getIndex(@RequestParam(value = "n") List<Integer> numbers)
+    {
+        int total = 0;
+        for (int i=0;i<numbers.size();i++) {
+            total += numbers.get(i);
+        }
+
+        return "the total is "+total;
+    }
+    @GetMapping("/math1/sum1")
+    public String getInvoiceDate(@RequestParam(value = "n") String number)
+    {
+        return "hello";
+    }
     @GetMapping("/math/pi")
     public String getPi()
     {
         return "3.141592653589793";
+    }
+//    @GetMapping("/math/volume/{length}/{width}/{height}")
+//    public String getVolumeOfRectangle()
+//    {
+//
+//    }
+
+
+
+    @GetMapping("/math/calculate")
+    public String calculate(
+            @RequestParam(value = "operation", required = false, defaultValue = "add") String operation,
+            @RequestParam(value = "x", required = true) int x,
+            @RequestParam(value = "y", required = true) int y
+
+    )
+    {
+        if(operation.toLowerCase().equals("add"))
+        {
+            int val = x + y;
+            return String.format("%s " + " + " + " %s = " + val + "", x, y);
+        }
+        else if( operation.toLowerCase().equals("multiply"))
+        {
+            int val = x * y;
+            return String.format("%s " + " * " + " %s = " + val + "", x, y);
+        }
+        else if( operation.toLowerCase().equals("subtract"))
+        {
+            int val = x - y;
+            return String.format("%s " + " - " + " %s = " + val + "", x, y);
+        }
+       else if (operation.toLowerCase().equals("divide"))
+        {
+            int val = x / y;
+            return String.format("%s " + " / " + " %s = " + val + "", x, y);
+        }
+       else return "null";
     }
 }
