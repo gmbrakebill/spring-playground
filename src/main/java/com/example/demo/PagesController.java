b0230aaf5606ef3a13a501d80d06f9ebf2279791
gmbrakebill <64878965+gmbrakebill@users.noreply.github.com>
2020-07-20 10:10:18 -0500

    with pi get route

--- src/main/java/com/example/demo/PagesController.java
+++ src/main/java/com/example/demo/PagesController.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PagesController {
-    @GetMapping("/hello")
-    public String hello()
+    @GetMapping("/math/pi")
+    public String getPi()
     {
-        return "hello world";
+        return "3.141592653589793";
     }
 }
